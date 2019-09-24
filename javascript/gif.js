c6d0e92bd9d00cf3c143b9bfa4081b1b29815ea6
Charissa Beaty <char.beaty11@gmail.com>
2019-09-23 18:55:08 -0600

    Added gif pictures to buttons

--- javascript/gif.js
+++ javascript/gif.js
@@ -1,41 +1,75 @@
-var APIKey = 'hPmUXlFapJzVK69XVJdatoUo20UetcjL'
 
-var queryURL = ''
-//*********STEP ONE**********/
 
-//create array of strings, each one related to a topic that interests me
-//save to a variable called 'topics
-var topics = ["Dog", "Cat", "Porcupine", "Giraffe", "Hippopotamus"];
-//***********STEP TWO*************/
+var topics = [];
 
-//topics in array should be made into buttons on HTML
-//use a loop that appends for each string in the array
+$("#add-buttons").on("click", "button", function() {
+
+    var input = $(this).attr("data-input");
+
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=hPmUXlFapJzVK69XVJdatoUo20UetcjL&limit=10';
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function (response) {
+        console.log(queryURL);
+
+        console.log(response);
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+
+
+            var pElement = $("<p>").text("Rating: " + results[i].rating);
+
+            var gifImage = $("<img>");
+
+            gifImage.attr("src", results[i].images.fixed_height.url);
+
+            $("#gifs-view").append(pElement);
+            $("#gifs-view").append(gifImage);
+
+        }
+
+    });
+});
+
 function showButtons() {
+    //deletes previous buttons
     $("#add-buttons").empty();
 
-    for(var i = 0; i < topics.length; i++) {
+    //loops through array of movies
+    for (var i = 0; i < topics.length; i++) {
+        //adding button element to html
         var b = $("<button>");
+        //adding a class to html
         b.addClass("animals");
-        b.attr("data-name", topics[i]);
+        //adding a data-attribute for value of topics at the index i
+        b.attr("data-input", topics[i]);
+        //adds text to button that was just added
         b.text(topics[i]);
-        $("#add-buttons").append(b);
-    }
+        //adds the buttons to html
+
+       
+        $("#add-buttons").append(b);  //append adds to what is already on the page
+    }                                 //it does not replace!
 }
 //***********STEP THREE*************/
 
-//When user clicks button, page should grab 10 static images
-$("#add-animal").on("click", function(event) {
 
+$("#add-animal").on("click", function (event) {
+    
     event.preventDefault();
-
+    //takes value from input box
     var input = $("#animal-input").val().trim();
-
+    //the input from the textbox is added to array
     topics.push(input);
-
+    //calling the function that handles the array
     showButtons();
 });
 
-showButtons();
+
 //***********STEP FOUR*************/
 
 //When user clicks on gif it should animate
@@ -43,15 +77,8 @@ showButtons();
 
 //***********STEP FIVE*************/
 
-//Dispaly rating under every gif
+//Dispaly gifRating under every gif
 
 //**********STOP! MAKE SURE IMAGES DISPLAY WHEN BUTTONS ARE PRESSED */
-
-//***********STEP SIX*************/
-
-//Add a form to page that takes value from a user input box and adds it
-//  to 'topics' array.
-//make a function call that takes each topic in array and remakes the 
-//  buttons on the page.
 
 
